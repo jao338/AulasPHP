24007e4934530ba3be48d72dd48b9375f33b4767
João Henrique <santos.joao@crmsolucoes.com.br>
2023-09-27 12:10:32 -0300

    3 - Tipos de dados 1/2

--- index.php
+++ index.php
@@ -9,11 +9,53 @@
     
     <?php
 
-        $bebida = "refrigerante";
+        $nome = 'Jão';
+        $idade = 22;
+        $altura = 1.73;
+        $bonito = false;
 
-        $$bebida = "suco";
 
-        echo $refrigerante;
+        echo var_dump($nome);
+
+        if(is_string(($nome))){
+            echo 'É uma string';
+        }else{
+            echo 'Não é uma string';
+        }
+
+        echo '<hr><br>';
+
+
+        echo var_dump($idade);
+
+        if(is_int($idade)){
+            echo 'É um int';
+        }else{
+            echo 'Não é um int';
+        }
+
+        echo '<hr><br>';
+
+
+        echo var_dump($altura);
+
+        if(is_float($altura)){
+            echo 'É um float';
+        }else{
+            echo 'Não é um float';
+        }
+
+        echo '<hr><br>';
+
+        echo var_dump($bonito);
+
+        if(is_bool($bonito)){
+            echo 'É um boolean';
+        }else{
+            echo 'Não é um boolean';
+        }
+
+        echo '<hr><br>';
 
     ?>
 
