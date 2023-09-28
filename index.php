253d76f167714bd318afe74ea58287b71c6ec6a2
João Henrique <santos.joao@crmsolucoes.com.br>
2023-09-28 09:17:23 -0300

    6 - Constante

--- index.php
+++ index.php
@@ -3,41 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula004</title>
+    <title>Aula005</title>
 </head>
 <body>
     
     <?php
 
-    $nome = 'Jão';
-    $a = 1;
-    $b = 3;
-    $c = 5;
+    define("NOME", "Jão");
 
-    function exibeNome(){
-        global $nome;
-        echo $nome;
-    }
-
-    function exibeCidade(){
-        global $cidade;
-        $cidade = "Rio Claro";
-    }
-
-    function soma(){
-        echo $GLOBALS['a'] + $GLOBALS['b'] + $GLOBALS['c'];
-    }
-
-    exibeNome();
-    echo "<hr>";
-
-    exibeCidade();
-    echo $cidade;
-    
-    echo "<hr>";
-
-    soma();
-
+    echo NOME;
     ?>
 
 </body>
