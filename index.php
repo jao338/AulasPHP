ff1e49b89363b2b18c6f575495f11fbb8d8806cb
João Henrique <santos.joao@crmsolucoes.com.br>
2023-09-28 08:43:12 -0300

    4 - Aspas e concatenaçação

--- index.php
+++ index.php
@@ -3,97 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula001</title>
+    <title>Aula003</title>
 </head>
 <body>
     
     <?php
 
-        $nome = 'Jão';
-        $idade = 22;
-        $altura = 1.73;
-        $bonito = false;
-        $array = array("Gol", "Uno", "Camaro", 20, 20.1, true);
-        $cidade = NULL;
+    $nome = 'Jão';
 
-
-        echo var_dump($nome);
-
-        if(is_string(($nome))){
-            echo 'É uma string';
-        }else{
-            echo 'Não é uma string';
-        }
-
-        echo '<hr><br>';
-
-
-        echo var_dump($idade);
-
-        if(is_int($idade)){
-            echo 'É um int';
-        }else{
-            echo 'Não é um int';
-        }
-
-        echo '<hr><br>';
-
-
-        echo var_dump($altura);
-
-        if(is_float($altura)){
-            echo 'É um float';
-        }else{
-            echo 'Não é um float';
-        }
-
-        echo '<hr><br>';
-
-        echo var_dump($bonito);
-
-        if(is_bool($bonito)){
-            echo 'É um boolean';
-        }else{
-            echo 'Não é um boolean';
-        }
-
-        echo '<hr><br>';
-
-        echo var_dump($array);
-
-        if(is_array($array)){
-            echo 'É um array';
-        }else{
-            echo 'Não é um array';
-        }
-
-        echo '<hr><br>';
-
-        class Cliente{
-            public $nome;
-
-            public function atribuirNome($nome){
-                $this->$nome = $nome;
-            }
-
-        }
-
-        $cliente = new Cliente();
-
-        $cliente->atribuirNome("Rodrigo");
-            
-        var_dump($cliente);
-
-        if(is_object($cliente)){
-            echo 'É um object';
-        }else{
-            echo 'Não é um object';
-        }
-
-        echo '<hr><br>';
-
-        echo var_dump($cidade);
-
+    echo $nome.'<br>';
+    echo "$nome<br>";
+    echo '$nome'.$nome.' e meu nome é \'20\'';
         
     ?>
 
