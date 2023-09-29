415a3ef9a4532992193af2088caeddf8450662ec
João Henrique <santos.joao@crmsolucoes.com.br>
2023-09-29 08:40:40 -0300

    12 - Operadores

---
+++ 12 - OPERADORES/operadoresAritmeticos.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula012 - Operadores</title>
+</head>
+<body>
+    
+    <?php
+    
+    $num1 = 10;
+    $num2 = 2;
+
+    echo $num1 + $num2 . "<br>";
+    echo $num1 - $num2 . "<br>";
+    echo $num1 * $num2 . "<br>";
+    echo $num1 / $num2 . "<br>";
+    echo $num1 % $num2 . "<br>";
+    echo $num1 ** $num2 . "<br>";
+    echo ($num1 + $num2) / $num2;
+
+    ?>
+
+</body>
+</html>
---
+++ 12 - OPERADORES/operadoresIncDec.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula012 - Operadores</title>
+</head>
+<body>
+    
+    <?php
+    
+    $pre = 20;
+    $pos = 10;
+
+    echo $pos . "<br>";
+
+    echo $pre++ . "<br>";
+
+    echo $pos . "<br><br>";
+
+    echo $pre . "<br>";
+
+    echo ++$pre . "<br>";
+
+    echo $pre . "<br>";
+
+    ?>
+
+</body>
+</html>
